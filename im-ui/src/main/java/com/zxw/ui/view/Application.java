2da0363d153e08d2762077880b29115bd1ed40d0
zhaoxiaowu <451863397@qq.com>
2021-01-04 00:26:59 +0800

    Login窗口

---
+++ im-ui/src/main/java/com/zxw/ui/view/Application.java
@@ -0,0 +1,18 @@
+package com.zxw.ui.view;
+
+import com.zxw.ui.view.login.Login;
+import javafx.stage.Stage;
+
+public class Application extends javafx.application.Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Login login = new Login();
+        login.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
